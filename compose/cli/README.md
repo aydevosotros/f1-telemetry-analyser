edc8e414ffba14f00e1f752b663dfb6ed4bbdf02
Antonio Molina <aydevosotros@gmail.com>
2023-06-28 11:38:04 +0200

    Up to date the project

--- compose/cli/README.md
+++
@@ -1,5 +0,0 @@
-# f1-2020-telemetry docker image
-
-## About
-
-It only makes ready a docker image with f1-2020-telemetry pip package installed
--- shipper/shipper.py
+++ shipper/shipper.py
@@ -1,11 +1,9 @@
-"""
-F1-Telemetry-analyser shipper is an scripts that listens
-to the UDP telemetry port from F1-2020 game and sends it
-to an influx database
-"""
 import logging
 import sys
 import click
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+from f1_23_telemetry.listener import TelemetryListener
 
 # Logging set-up
 logger = logging.getLogger('shipper')
@@ -13,10 +11,53 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.setLevel(logging.INFO)
 
 
+class PackageRepo:
+    def __init__(self, influx_host, influx_port, influx_db, influx_token):
+        self.influx_host = influx_host
+        self.influx_port = influx_port
+        self.influx_db = influx_db
+        self.influx_token = influx_token
+
+        self.influx_client = InfluxDBClient(
+            url=f"http://{influx_host}:{influx_port}",
+            token=influx_token,
+            org="-")
+
+        self.write_api = self.influx_client.write_api(
+            write_options=SYNCHRONOUS
+        )
+
+    def write_package(self, package):
+        json_body = [
+            {
+                "measurement": "f1_telemetry",
+                "tags": {
+                    "car_number": package['car_number']
+                },
+                "fields": package
+            }
+        ]
+        self.write_api.write(self.influx_db, self.influx_org, json_body)
+
+
 @click.command()
 def ship():
     logger.info('Starting package shipping')
-
+    listener = TelemetryListener(port=20777, host='localhost')
+    package_repo = PackageRepo(
+        influx_host='influxdb',
+        influx_db='f1_telemetry',
+        influx_port=8086,
+        influx_token=None
+    )
+    while True:
+        try:
+            package = listener.get()
+            logger.info(f'Received packet: {package}')
+            package_repo.write_package(package)
+        except KeyboardInterrupt:
+            logger.info("Exiting")
+    
 
 if __name__ == '__main__':
     ship()
